2e0587ad3bfbd8177c1ef300368d47b63095853a
Andy Chan <andy.chan9930@gmail.com>
2021-11-09 17:47:26 -0500

    done

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+function saturdayFun (activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}`
 }
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+console.log(saturdayFun())
+
+function mondayWork(activity = "work out") {
+  return `This Monday, I will ${activity}!`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+console.log(mondayWork())
+
+function wrapAdjective () {
+  return function (string = "special") {
+    return `You are ${string}`
   }
 }
+
+console.log(wrapAdjective()())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function saturdayFun(activity="roller-skate") {
+//   return `This Saturday, I want to ${activity}!`
+// }
+
+// let mondayWork = function(activity="go to the office") {
+//   return `This Monday, I will ${activity}.`
+// }
+
+// let wrapAdjective = function(style="*") {
+//   return function(adjective="special") {
+//     return `You are ${style}${adjective}${style}!`
+//   }
+// }
